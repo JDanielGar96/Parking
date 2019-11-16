6d8e2c6a4222ea69248c87c0c659a89645bd09d6
Jose García <josedgarciad96@gmail.com>
2019-11-15 20:22:52 -0500

    Create first structure of bean

---
+++ src/com/unbosque/beans/UsuarioBean.java
@@ -0,0 +1,12 @@
+package com.unbosque.beans;
+
+import java.util.Date;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+public class UsuarioBean {
+	
+}
